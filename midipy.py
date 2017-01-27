cba6a29a0c717442045f86b17e517e1b6b5e3c4f
ckjbgames <ckjbgames@gmail.com>
2017-01-26 20:10:02 -0500

    Update midipy.py

--- midipy.py
+++ midipy.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # midilib.py
+def hexchr(hexa):
+    return chr(int(hexa,16))
 class midi(object):
+    # Event definitions (in hexadecimal)
+    trackevents = {'seq_number' : '00',
+                   'txtevent' : '01',
+                   'trackname' : '03',
+                   'instrument' : '04'}
     def __init__(self, fname, tracks):
         self.midifile = open(fname,'a+')
-        self.midifile.write
+        self.midifile.write('MThd')
